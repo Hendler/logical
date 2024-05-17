3713311dcd854ab2108b91b027f32b7b2962bdc6
Ubuntu <ubuntu@ip-10-240-180-36.us-west-2.compute.internal>
2024-05-17 20:52:52 +0000

    Fix AttributeError by adding task decorator to run_logic_task

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,10 +1,10 @@
 from dotenv import load_dotenv, find_dotenv
+from invoke import Collection, task
 
 load_dotenv(find_dotenv())
 
-from invoke import Collection
 from .tasks import parse, run_logic_task
 
 ns = Collection()
 ns.add_task(parse)
-ns.add_task(run_logic_task)
+ns.add_task(run_logic_task, name='run-logic-task')
